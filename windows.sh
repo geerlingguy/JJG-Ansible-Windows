cddb50e0bb6c3cbb8846fbc74c8444d286003f42
Jeff Geerling <geerlingguy@mac.com>
2014-12-18 13:30:54 -0600

    Issue #7: Make it work on Ubuntu/Debian and apt-based distros.

--- windows.sh
+++ windows.sh
@@ -5,13 +5,17 @@
 #
 # @see README.md
 # @author Jeff Geerling, 2014
-# @version 1.0
 
 # Uncomment if behind a proxy server.
 # export {http,https,ftp}_proxy='http://username:password@proxy-host:80'
 
 ANSIBLE_PLAYBOOK=$1
 
+# Detect package management system.
+YUM=$(which yum)
+APT_GET=$(which apt-get)
+
+# Make sure Ansible playbook exists.
 if [ ! -f /vagrant/$ANSIBLE_PLAYBOOK ]; then
   echo "Cannot find Ansible playbook."
   exit 1
@@ -20,15 +24,25 @@ fi
 # Install Ansible and its dependencies if it's not installed already.
 if [ ! -f /usr/bin/ansible ]; then
   echo "Installing Ansible dependencies and Git."
-  yum install -y git python python-devel
+  if [[ ! -z $YUM ]]; then
+    yum install -y git python python-devel
+  elif [[ ! -z $APT_GET ]]; then
+    apt-get install -y git python python-devel
+  else
+    echo "Neither yum nor apt-get are available."
+    exit 1;
+  fi
+
   echo "Installing pip via easy_install."
   wget http://peak.telecommunity.com/dist/ez_setup.py
   python ez_setup.py && rm -f ez_setup.py
   easy_install pip
   # Make sure setuptools are installed crrectly.
   pip install setuptools --no-use-wheel --upgrade
+
   echo "Installing required python modules."
   pip install paramiko pyyaml jinja2 markupsafe
+
   echo "Installing Ansible."
   pip install ansible
 fi
